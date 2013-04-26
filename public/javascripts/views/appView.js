10ac669f1e42b99a23f8f5844767c2d9167b894a
nachiket-synerzip <nachiket.kakatkar@synerzip.com>
2013-04-26 16:41:07 +0530

    Added HTML contents to the tabs. Added CSS file and templates. TODO: improve CSS

--- public/javascripts/views/appView.js
+++ public/javascripts/views/appView.js
@@ -1,59 +1,78 @@
-define([ 'jquery', 'jquery_ui', 'underscore', 'backbone', 'text!templates/appTpl.html', 'collections/contacts', 'models/contact', 'views/contactView'],
-		function($, ui, _, Backbone, appTemplate, ContactCollection, Contact, ContactView) {
+define([ 'jquery', 'jquery_ui', 'underscore', 'backbone', 'text!templates/appTpl.html', 'collections/contacts', 'models/contact', 'views/contactView', 'text!templates/contactNewTpl.html', 'views/contactDelView'],
+		function($, ui, _, Backbone, appTemplate, ContactCollection, Contact, ContactView, newContactForm, DelContactView) {
 
 	var appView = Backbone.View.extend({
 
-		template : _.template(appTemplate),
+		template: _.template(appTemplate),
 
 		initialize: function(){
 		},
 
+		contacts: {},
+
 		render: function() {
 			this.$el.html(this.template());
-			//console.log("Should render contacts");
-			//this.renderContacts();
-			//render tabs
 			$(function(){
 				console.log($('#contacts-tabs'));
 				$('#contacts-tabs').tabs();
 			});
+			this.renderContacts();
+			this.renderNewContactsForm();
+			this.renderDeleteContactsForm();
 			return this;
 		},
 		
+		renderDeleteContactsForm: function() {
+			this.contacts.each(function(contact){
+				var contactView = new DelContactView({
+					model: contact
+				});
+				var a = contactView.render().el;
+				console.log(a);
+				$("#del-contacts-list").append(a);
+			});
+		},
+
+		renderNewContactsForm: function(){
+			console.log(newContactForm);
+			$("#tabs-new").html(newContactForm);
+		},
 		renderContacts: function(){
-			this.$("#contacts-list").html("");
-			var contacts = new ContactCollection([
+			$("#all-contacts-list").html("");
+			this.contacts = new ContactCollection([
 				{
 					name: "Nachiket Kakatkar",
-					mobile: 9766125893,
+					mobile: 1234567891,
 					email: "nachiket.kakatkar@synerzip.com",
 					address: "Pune"
 				},
 				{
 					name: "Nachiket2 Kakatkar2",
-					mobile: 9766125892,
+					mobile: 1234567892,
 					email: "nachiket.kakatkar2@synerzip.com",
 					address: "Pune2"					
 				},
 				{
 					name: "Nachiket Kakatkar3",
-					mobile: 97661258933,
+					mobile: 1234567893,
 					email: "nachiket.kakatkar3@synerzip.com",
 					address: "Pune3"					
 				},
 				{
 					name: "Nachiket Kakatkar4",
-					mobile: 97661258934,
+					mobile: 1234567894,
 					email: "nachiket.kakatkar4@synerzip.com",
 					address: "Pune4"					
 				}
 			]);
-			console.log(contacts);
-			contacts.each(function(contact){
+			console.log(this.contacts);
+			this.contacts.each(function(contact){
 				var contactView = new ContactView({
 					model: contact
 				});
-				this.$("#contacts-list").append(contactView.render().el);
+				var a = contactView.render().el;
+				console.log(a);
+				$("#all-contacts-list").append(a);
 			});
 		}
 	});
--- public/javascripts/views/contactDelView.js
+++ public/javascripts/views/contactDelView.js
@@ -1,13 +1,13 @@
-define([ 'jquery', 'underscore', 'backbone', 'text!templates/contactTpl.html' ],
-		function($, _, Backbone, contactTemplate) {
+define([ 'jquery', 'underscore', 'backbone', 'text!templates/contactDelTpl.html' ],
+		function($, _, Backbone, contactDelTemplate) {
 
 	var contactView = Backbone.View.extend({
 		tagName: 'li',
 		className: 'contact-list-item',
-		template : _.template(contactTemplate),
+		template : _.template(contactDelTemplate),
 		
 		render: function() {
-			console.log("rendering contact ");
+			console.log("rendering del contact ");
 			console.log(this.model.toJSON());
 			this.$el.html(this.template(this.model.toJSON()));
 			return this;
